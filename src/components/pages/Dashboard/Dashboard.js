7d42dcdbddbd1f4b56dfcf99c516924f7c46ba7f
angelicavc7 <angelicavillar_c@live.com>
2021-05-27 12:35:49 -0700

    working forms and pages

---
+++ src/components/pages/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashboard() {
+    
+    return (
+        <div className='Dashboard'>
+            <h1>this is the first page to wour website find paws</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
