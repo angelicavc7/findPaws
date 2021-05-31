cce5832407c294d9f4ce022aae8bfb9e4f2731fb
Juan Vazquez <ehulises28@hotmail.com>
2021-05-31 15:21:40 -0700

    found

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -22,7 +22,7 @@ function Dashboard() {
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
         </Breadcrumb>
-        <div className="site-layout-content">Content</div>
+        <div className="site-layout-content"></div>
       </Content>
       <Footer style={{ textAlign: "center" }}>
         Ant Design ©2018 Created by Ant UED
