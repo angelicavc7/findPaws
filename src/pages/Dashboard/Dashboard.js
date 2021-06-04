fb7053765402c01bfeb8615bf38e3209155d02b2
Juan Vazquez <ehulises28@hotmail.com>
2021-06-04 12:16:01 -0700

    fixed cards and moved side to the right

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,14 +4,14 @@ import "./Dashboard.css";
 import { Layout } from "antd";
 import Header from "../../components/Header/Header";
 import Card from "../../components/Card/Index";
-import { Carousel } from 'antd';
+import { Carousel } from "antd";
 
 const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
+  height: "500px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
 };
 
 const { Content, Footer, Sider } = Layout;
@@ -22,36 +22,33 @@ function Dashboard() {
       <Layout className="layout">
         <Header></Header>
         <Layout>
+          <Content style={{ padding: "0 50px" }}>
+            <div className="site-layout-content">
+              <h1>Find your pet!!!!</h1>
+
+              <Carousel autoplay>
+                <div>
+                  <h3 style={contentStyle}>1</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>2</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>3</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>4</h3>
+                </div>
+              </Carousel>
+            </div>
+          </Content>
           <Sider>
             <Card />
           </Sider>
-
-          <Content style={{ padding: "0 50px" }}>
-            <div className="site-layout-content">
-              <h1>Find your child!</h1>
-
-            <Carousel autoplay>
-          <div>
-            <h3 style={contentStyle}>1</h3>
-          </div>
-         <div>
-        <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-        <h3 style={contentStyle}>3</h3>
-        </div>
-       <div>
-         <h3 style={contentStyle}>4</h3>
-        </div>
-          </Carousel>
-
-          </div>
-          </Content>
         </Layout>
-        <Footer style={{ textAlign: "center" }}></Footer>
+        <Footer style={{ textAlign: "center" }}>find your pet</Footer>
       </Layout>
     </div>
-    // document.getElementById("container")
   );
 }
 export default Dashboard;
