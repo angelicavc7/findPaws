e93011f6b9bcb1bfd2fd4b5c194317be3e24422c
angelicavc7 <angelicavillar_c@live.com>
2021-06-04 11:13:03 -0700

    addchangs

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,6 +4,16 @@ import "./Dashboard.css";
 import { Layout } from "antd";
 import Header from "../../components/Header/Header";
 import Card from "../../components/Card/Index";
+import { Carousel } from 'antd';
+
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
 const { Content, Footer, Sider } = Layout;
 
 function Dashboard() {
@@ -19,7 +29,23 @@ function Dashboard() {
           <Content style={{ padding: "0 50px" }}>
             <div className="site-layout-content">
               <h1>Find your child!</h1>
-            </div>
+
+            <Carousel autoplay>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+         <div>
+        <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+        <h3 style={contentStyle}>3</h3>
+        </div>
+       <div>
+         <h3 style={contentStyle}>4</h3>
+        </div>
+          </Carousel>
+
+          </div>
           </Content>
         </Layout>
         <Footer style={{ textAlign: "center" }}></Footer>
