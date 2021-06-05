65f87e9852f5d31455bf4b12d1f5837d2b0ad1dc
Juan Vazquez <ehulises28@hotmail.com>
2021-06-04 18:21:17 -0700

    worked on forms

--- src/components/Form/FormSizeDemo.js
+++ src/components/Form/FormSizeDemo.js
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
 import "antd/dist/antd.css";
-import {
-  Form,
-  Input,
-  Button,
-  Select,
-  DatePicker,
-  InputNumber,
-  Upload,
-  message,
-} from "antd";
+import { Form, Input, Button, Select, DatePicker, Upload, message } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
+import API from "../../utils/API";
 
 const FormSizeDemo = () => {
   const [componentSize, setComponentSize] = useState("default");
@@ -36,9 +28,42 @@ const FormSizeDemo = () => {
       }
     },
   };
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    petName: "",
+    petType: "",
+    petDescription: "",
+    lastSeen: "",
+    dateLost: "",
+    reward: "",
+  });
+  const formSubmit = (e) => {
+    e.preventDefault();
+    API.getAllLostPets(formState).then((res) => {
+      console.log(res.data);
+      props.fetchData();
+      setFormState({
+        firstName: "",
+        lastName: "",
+        phoneNumber: "",
+        email: "",
+        petName: "",
+        petType: "",
+        petDescription: "",
+        lastSeen: "",
+        dateLost: "",
+        reward: "",
+      });
+      console.log(formSubmit);
+    });
+  };
   return (
     <>
       <Form
+        onSubmit={formSubmit}
         labelCol={{
           span: 4,
         }}
@@ -53,43 +78,111 @@ const FormSizeDemo = () => {
         size={componentSize}
       >
         <Form.Item label="First Name ">
-          <Input />
+          <Input
+            value={formState.firstName}
+            type="firstName"
+            onChange={(e) =>
+              setFormState({ ...formState, firstName: e.target.value })
+            }
+          />
         </Form.Item>
+
         <Form.Item label="Last Name ">
-          <Input />
+          <Input
+            value={formState.lastName}
+            type="lastName"
+            onChange={(e) =>
+              setFormState({ ...formState, lastName: e.target.value })
+            }
+          />
         </Form.Item>
-        <Form.Item label="Phone Num">
-          <Input />
+
+        <Form.Item label="Phone Number">
+          <Input
+            value={formState.phoneNumber}
+            type="phoneNumber"
+            onChange={(e) =>
+              setFormState({ ...formState, phoneNumber: e.target.value })
+            }
+          />
         </Form.Item>
+
         <Form.Item label="Email">
-          <Input />
+          <Input
+            value={formState.email}
+            type="email"
+            onChange={(e) =>
+              setFormState({ ...formState, email: e.target.value })
+            }
+          />
         </Form.Item>
+
         <Form.Item label="Pet Name">
-          <Input />
+          <Input
+            value={formState.petName}
+            type="petName"
+            ç
+            onChange={(e) =>
+              setFormState({ ...formState, petName: e.target.value })
+            }
+          />
         </Form.Item>
+
         <Form.Item label="Select Type Of Pet ">
           <Select>
-            <Select.Option value="Dog">Dog</Select.Option>
+            <Select.Option
+              value={formState.petType}
+              type="petType"
+              onChange={(e) =>
+                setFormState({ ...formState, petType: e.target.value })
+              }
+            >
+              Dog
+            </Select.Option>
             <Select.Option value="Cat">Cat</Select.Option>
           </Select>
         </Form.Item>
+
         <Form.Item label="Pet Description">
-          <Input />
+          <Input
+            value={formState.petDescription}
+            type="petDescription"
+            onChange={(e) =>
+              setFormState({ ...formState, petDescription: e.target.value })
+            }
+          />
         </Form.Item>
+
         <Form.Item label="Last Seen Location">
-          <Input />
+          <Input
+            value={formState.lastSeen}
+            type="lastSeen"
+            onChange={(e) =>
+              setFormState({ ...formState, lastSeen: e.target.value })
+            }
+          />
         </Form.Item>
+
         <Form.Item label="Date Lost">
           <DatePicker />
         </Form.Item>
+
         <Form.Item label="Reward">
-          <InputNumber />
+          <Input
+            value={formState.reward}
+            type="reward"
+            onChange={(e) =>
+              setFormState({ ...formState, reward: e.target.value })
+            }
+          />
         </Form.Item>
+
         <Form.Item>
           <Upload {...props}>
             <Button icon={<UploadOutlined />}>Upload Image</Button>
           </Upload>
         </Form.Item>
+
         <Form.Item>
           <Button type="primary">Post Pet</Button>
         </Form.Item>
